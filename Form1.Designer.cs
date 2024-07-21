3ea53609662eadf7a9ce3f490ce3df440d4dec13
saveen-maduranga <saveenonline@gmail.com>
2024-07-21 21:26:17 +0530

    Update Files

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CsharpCalc
 {
-    partial class Form1
+    partial class Calculator
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,18 +28,325 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Calculator));
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
             SuspendLayout();
             // 
-            // Form1
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(12, 25);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(215, 38);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "0";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.LightSkyBlue;
+            button1.Cursor = Cursors.Hand;
+            button1.FlatAppearance.BorderColor = Color.DeepSkyBlue;
+            button1.FlatAppearance.MouseOverBackColor = Color.DodgerBlue;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 15.75F);
+            button1.Location = new Point(12, 86);
+            button1.Name = "button1";
+            button1.Size = new Size(50, 41);
+            button1.TabIndex = 1;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.LightSkyBlue;
+            button2.Cursor = Cursors.Hand;
+            button2.FlatAppearance.BorderColor = Color.DeepSkyBlue;
+            button2.FlatAppearance.MouseOverBackColor = Color.DodgerBlue;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 15.75F);
+            button2.Location = new Point(67, 86);
+            button2.Name = "button2";
+            button2.Size = new Size(50, 41);
+            button2.TabIndex = 2;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.LightSkyBlue;
+            button3.Cursor = Cursors.Hand;
+            button3.FlatAppearance.BorderColor = Color.DeepSkyBlue;
+            button3.FlatAppearance.MouseOverBackColor = Color.DodgerBlue;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI", 15.75F);
+            button3.Location = new Point(122, 86);
+            button3.Name = "button3";
+            button3.Size = new Size(50, 41);
+            button3.TabIndex = 3;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.LightSkyBlue;
+            button4.Cursor = Cursors.Hand;
+            button4.FlatAppearance.BorderColor = Color.DeepSkyBlue;
+            button4.FlatAppearance.MouseOverBackColor = Color.DodgerBlue;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Segoe UI", 15.75F);
+            button4.Location = new Point(177, 86);
+            button4.Name = "button4";
+            button4.Size = new Size(50, 41);
+            button4.TabIndex = 4;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.LightSkyBlue;
+            button5.Cursor = Cursors.Hand;
+            button5.FlatAppearance.BorderColor = Color.DeepSkyBlue;
+            button5.FlatAppearance.MouseOverBackColor = Color.DodgerBlue;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Segoe UI", 15.75F);
+            button5.Location = new Point(177, 133);
+            button5.Name = "button5";
+            button5.Size = new Size(50, 41);
+            button5.TabIndex = 8;
+            button5.Text = "8";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.LightSkyBlue;
+            button6.Cursor = Cursors.Hand;
+            button6.FlatAppearance.BorderColor = Color.DeepSkyBlue;
+            button6.FlatAppearance.MouseOverBackColor = Color.DodgerBlue;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Font = new Font("Segoe UI", 15.75F);
+            button6.Location = new Point(122, 133);
+            button6.Name = "button6";
+            button6.Size = new Size(50, 41);
+            button6.TabIndex = 7;
+            button6.Text = "7";
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.LightSkyBlue;
+            button7.Cursor = Cursors.Hand;
+            button7.FlatAppearance.BorderColor = Color.DeepSkyBlue;
+            button7.FlatAppearance.MouseOverBackColor = Color.DodgerBlue;
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Font = new Font("Segoe UI", 15.75F);
+            button7.Location = new Point(67, 133);
+            button7.Name = "button7";
+            button7.Size = new Size(50, 41);
+            button7.TabIndex = 6;
+            button7.Text = "6";
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.LightSkyBlue;
+            button8.Cursor = Cursors.Hand;
+            button8.FlatAppearance.BorderColor = Color.DeepSkyBlue;
+            button8.FlatAppearance.MouseOverBackColor = Color.DodgerBlue;
+            button8.FlatStyle = FlatStyle.Flat;
+            button8.Font = new Font("Segoe UI", 15.75F);
+            button8.Location = new Point(12, 133);
+            button8.Name = "button8";
+            button8.Size = new Size(50, 41);
+            button8.TabIndex = 5;
+            button8.Text = "5";
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.LightSkyBlue;
+            button9.Cursor = Cursors.Hand;
+            button9.FlatAppearance.BorderColor = Color.DeepSkyBlue;
+            button9.FlatAppearance.MouseOverBackColor = Color.DodgerBlue;
+            button9.FlatStyle = FlatStyle.Flat;
+            button9.Font = new Font("Segoe UI", 15.75F);
+            button9.Location = new Point(177, 180);
+            button9.Name = "button9";
+            button9.Size = new Size(50, 41);
+            button9.TabIndex = 12;
+            button9.Text = "=";
+            button9.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            button10.BackColor = Color.LightSkyBlue;
+            button10.Cursor = Cursors.Hand;
+            button10.FlatAppearance.BorderColor = Color.DeepSkyBlue;
+            button10.FlatAppearance.MouseOverBackColor = Color.DodgerBlue;
+            button10.FlatStyle = FlatStyle.Flat;
+            button10.Font = new Font("Segoe UI", 15.75F);
+            button10.Location = new Point(122, 180);
+            button10.Name = "button10";
+            button10.Size = new Size(50, 41);
+            button10.TabIndex = 11;
+            button10.Text = ".";
+            button10.UseVisualStyleBackColor = false;
+            // 
+            // button11
+            // 
+            button11.BackColor = Color.LightSkyBlue;
+            button11.Cursor = Cursors.Hand;
+            button11.FlatAppearance.BorderColor = Color.DeepSkyBlue;
+            button11.FlatAppearance.MouseOverBackColor = Color.DodgerBlue;
+            button11.FlatStyle = FlatStyle.Flat;
+            button11.Font = new Font("Segoe UI", 15.75F);
+            button11.Location = new Point(67, 180);
+            button11.Name = "button11";
+            button11.Size = new Size(50, 41);
+            button11.TabIndex = 10;
+            button11.Text = "0";
+            button11.UseVisualStyleBackColor = false;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.LightSkyBlue;
+            button12.Cursor = Cursors.Hand;
+            button12.FlatAppearance.BorderColor = Color.DeepSkyBlue;
+            button12.FlatAppearance.MouseOverBackColor = Color.DodgerBlue;
+            button12.FlatStyle = FlatStyle.Flat;
+            button12.Font = new Font("Segoe UI", 15.75F);
+            button12.Location = new Point(12, 180);
+            button12.Name = "button12";
+            button12.Size = new Size(50, 41);
+            button12.TabIndex = 9;
+            button12.Text = "9";
+            button12.UseVisualStyleBackColor = false;
+            // 
+            // button13
+            // 
+            button13.BackColor = Color.LightSkyBlue;
+            button13.Cursor = Cursors.Hand;
+            button13.FlatAppearance.BorderColor = Color.DeepSkyBlue;
+            button13.FlatAppearance.MouseOverBackColor = Color.DodgerBlue;
+            button13.FlatStyle = FlatStyle.Flat;
+            button13.Font = new Font("Segoe UI", 15.75F);
+            button13.Location = new Point(177, 227);
+            button13.Name = "button13";
+            button13.Size = new Size(50, 41);
+            button13.TabIndex = 16;
+            button13.Text = "/";
+            button13.UseVisualStyleBackColor = false;
+            // 
+            // button14
+            // 
+            button14.BackColor = Color.LightSkyBlue;
+            button14.Cursor = Cursors.Hand;
+            button14.FlatAppearance.BorderColor = Color.DeepSkyBlue;
+            button14.FlatAppearance.MouseOverBackColor = Color.DodgerBlue;
+            button14.FlatStyle = FlatStyle.Flat;
+            button14.Font = new Font("Segoe UI", 15.75F);
+            button14.Location = new Point(122, 227);
+            button14.Name = "button14";
+            button14.Size = new Size(50, 41);
+            button14.TabIndex = 15;
+            button14.Text = "x";
+            button14.UseVisualStyleBackColor = false;
+            // 
+            // button15
+            // 
+            button15.BackColor = Color.LightSkyBlue;
+            button15.Cursor = Cursors.Hand;
+            button15.FlatAppearance.BorderColor = Color.DeepSkyBlue;
+            button15.FlatAppearance.MouseOverBackColor = Color.DodgerBlue;
+            button15.FlatStyle = FlatStyle.Flat;
+            button15.Font = new Font("Segoe UI", 15.75F);
+            button15.Location = new Point(67, 227);
+            button15.Name = "button15";
+            button15.Size = new Size(50, 41);
+            button15.TabIndex = 14;
+            button15.Text = "-";
+            button15.UseVisualStyleBackColor = false;
+            // 
+            // button16
+            // 
+            button16.BackColor = Color.LightSkyBlue;
+            button16.Cursor = Cursors.Hand;
+            button16.FlatAppearance.BorderColor = Color.DeepSkyBlue;
+            button16.FlatAppearance.MouseOverBackColor = Color.DodgerBlue;
+            button16.FlatStyle = FlatStyle.Flat;
+            button16.Font = new Font("Segoe UI", 15.75F);
+            button16.Location = new Point(12, 227);
+            button16.Name = "button16";
+            button16.Size = new Size(50, 41);
+            button16.TabIndex = 13;
+            button16.Text = "+";
+            button16.UseVisualStyleBackColor = false;
+            // 
+            // Calculator
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(278, 272);
-            Name = "Form1";
-            Text = "Form1";
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(241, 290);
+            Controls.Add(button13);
+            Controls.Add(button14);
+            Controls.Add(button15);
+            Controls.Add(button16);
+            Controls.Add(button9);
+            Controls.Add(button10);
+            Controls.Add(button11);
+            Controls.Add(button12);
+            Controls.Add(button5);
+            Controls.Add(button6);
+            Controls.Add(button7);
+            Controls.Add(button8);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Calculator";
+            Text = "Cal S99";
+            Load += Calculator_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,114 @@
 namespace CsharpCalc
 {
-    public partial class Form1 : Form
+    public partial class Calculator : Form
     {
-        public Form1()
+        public Calculator()
         {
             InitializeComponent();
+        }
+
+        float num1 = 0, num2 = 0;
+        int oprClickCount = 0;
+        bool isOprClick = false, isEqualClick = false;
+        string opr;
+
+        private void Calculator_Load(object sender, EventArgs e)
+        {
+            // add button click event to all buttons
+            foreach (Control c in Controls)
+            {
+                if (c is Button)
+                {
+                    c.Click += new System.EventHandler(btn_click);
+                }
+            }
+        }
+
+        // create button click event
+        public void btn_click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if (!isOperator(button)) // if the button is a anumber
+            {
+                // if the textbox text not contain "."
+                if (textBox1.Text.Contains("."))
+                {
+                    if (textBox1.Text.Equals("0") && !button.Text.Equals("."))
+                    {
+                        // delete the first "0"
+                        // set button text to the textbox
+                        // if the button text is not "."
+                        textBox1.Text = button.Text;
+                        isOprClick = false;
+                    }
+                    else
+                    {
+                        textBox1.Text += button.Text;
+                        isOprClick = false;
+                    }
+
+                }
+                else if (!button.Text.Equals("."))
+                {
+                    textBox1.Text += button.Text;
+                    isOprClick = false;
+                }
+            }
+            else // if the button is an operator
+            {
+                if (oprClickCount == 0) // if it's a first time we click on a operator
+                {
+                    oprClickCount ++;
+                    // convrt textbox text to float and set it into num1
+                    num1 = float.Parse(textBox1.Text);
+                    // get the operator from button text
+                    opr = button.Text;
+                    // set oprClick to true
+                    isOprClick = true;
+                }
+            }
+        }
+
+        // create a function to check if the clicked button is a number or an operator
+        public bool isOperator(Button button)
+        {
+            string buttonText = button.Text;
+
+            if (buttonText.Equals("+") || buttonText.Equals("-") ||
+                buttonText.Equals("x") || buttonText.Equals("/") ||
+                buttonText.Equals("="))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // function to calculate
+        public float calc(string opr, float n1, float n2) 
+        {
+            float result = 0;
+
+            switch (opr) 
+            {
+                case "+":
+                    result = n1 + n2; 
+                    break;
+                case "-":
+                    result = n1 - n2;
+                    break;
+                case "x":
+                    result = n1 * n2;
+                    break;
+                case "/":
+                    if (n2 != 0)
+                        result = n1 / n2;
+                    break;
+            }
+            return result;
         }
     }
 }
